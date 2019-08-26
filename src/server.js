a2526aaebbc0780c584d56bb0e07f34603f2d6e2
Bryce Eklund <bryce.eklund@gmail.com>
2019-08-26 15:24:29 -0700

    configure new relic

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const app = require('../src/app');
 const { PORT } = require('./config');
-require('newrelic');
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}.`)
